a5097b060de86b8ebb1fcfd9e971426cfaba053c
jake matillano <jakematillano.z@gmail.com>
2022-09-07 09:44:06 -0700

    01 done

--- 3day/01-react-hooks-useState/src/components/UseState/index.js
+++ 3day/01-react-hooks-useState/src/components/UseState/index.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import "./UseState.css";
+
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(window.localStorage.getItem("item") || 0);
+
   return (
-    <div>
+    <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
+      {/* <button onClick={() => setTheme('light')}>Light</button> */}
+      {/* <button onClick={() => setTheme('dark')}>Dark</button> */}
+      <button
+        onClick={() =>
+          theme === "dark" ? setTheme("light") : setTheme("dark")
+        }
+      >
+        Toggle Theme
+      </button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
     </div>
   );
 };
