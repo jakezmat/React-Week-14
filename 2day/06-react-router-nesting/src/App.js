5b52cfdcc8a18cf7d5d316cf168fd3ca9326881f
jake matillano <jakematillano.z@gmail.com>
2022-09-06 20:09:03 -0700

    movies component done

--- 2day/06-react-router-nesting/src/App.js
+++ 2day/06-react-router-nesting/src/App.js
@@ -2,7 +2,7 @@ import { Route, Switch, NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
-
+import { movies } from "./data/movieData"
 function App() {
   return (
     <div className='main'>
