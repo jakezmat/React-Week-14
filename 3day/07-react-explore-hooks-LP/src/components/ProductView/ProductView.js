ca894e34c6f44f3a5187248122a7ab21e23fa0be
jake matillano <jakematillano.z@gmail.com>
2022-09-07 13:48:35 -0700

    1st phase done

--- 3day/07-react-explore-hooks-LP/src/components/ProductView/ProductView.js
+++ 3day/07-react-explore-hooks-LP/src/components/ProductView/ProductView.js
@@ -1,4 +1,9 @@
+
 import React from 'react';
+import { useState, useEffect } from "react";
+
+// import { useHistory } from "react-router-dom";
+
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
@@ -6,7 +11,7 @@ import './ProductView.css'
 function ProductView({ products }) {
 
   // TODO: Replace with state variable
-  const sideOpen = true;
+  const [sideOpen, setSideOpen] = useState(true);
 
   return (
     <div className="product-view">
@@ -25,7 +30,8 @@ function ProductView({ products }) {
       <div className="product-side-panel">
         <div className="product-side-panel-toggle-wrapper">
           <div className="product-side-panel-toggle"
-               onClick={() => console.log('TOGGLE SIDE PANEL')}>
+               onClick={() =>
+               sideOpen===true ? setSideOpen(false):setSideOpen(true)}>
             {sideOpen ? '>' : '<'}
           </div>
         </div>
