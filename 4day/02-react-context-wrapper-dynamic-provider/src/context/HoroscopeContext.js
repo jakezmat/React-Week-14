166a740e72c9a711da75e70f8271ed60e1abdf1c
jake matillano <jakematillano.z@gmail.com>
2022-09-08 09:06:46 -0700

    dynamic context done

--- 4day/02-react-context-wrapper-dynamic-provider/src/context/HoroscopeContext.js
+++ 4day/02-react-context-wrapper-dynamic-provider/src/context/HoroscopeContext.js
@@ -1,10 +1,14 @@
-import { createContext } from 'react';
+import { createContext, useState } from "react";
+import horoscopeObj from "../data/horoscopes";
 
 export const HoroscopeContext = createContext();
 
-const HoroscopeProvider = props => {
+const HoroscopeProvider = (props) => {
+  const [currentSign, setCurrentSign] = useState("Pisces");
+  const sign = horoscopeObj[currentSign];
+
   return (
-    <HoroscopeContext.Provider>
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
       {props.children}
     </HoroscopeContext.Provider>
   );
--- 4day/02-react-context-wrapper-dynamic-provider/src/index.js
+++ 4day/02-react-context-wrapper-dynamic-provider/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HoroscopeContext } from './context/HoroscopeContext';
-
+import HoroscopeProvider from "./context/HoroscopeContext";
 const Root = () => {
   return (
     <HoroscopeProvider>
