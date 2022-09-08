16673122020e57479cf6d7e2f87ec24086a9db50
jake matillano <jakematillano.z@gmail.com>
2022-09-08 09:22:27 -0700

    details.js updated

--- 4day/03-react-context-wrapper-dynamic-consumer/src/components/Navbar.js
+++ 4day/03-react-context-wrapper-dynamic-consumer/src/components/Navbar.js
@@ -1,17 +1,20 @@
-import horoscopesObj from '../data/horoscopes';
+import { useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
+import horoscopesObj from "../data/horoscopes";
 
 const Navbar = () => {
+  const { setCurrentSign } = useContext(HoroscopeContext);
   const horoscopes = Object.keys(horoscopesObj);
 
   return (
     <nav>
-      {horoscopes.map(sign => (
-        <span key={sign}>
-        {sign}
+      {horoscopes.map((sign) => (
+        <span key={sign} onClick={() => setCurrentSign(sign)}>
+          {sign}
         </span>
       ))}
     </nav>
-  )
+  );
 };
 
 export default Navbar;
