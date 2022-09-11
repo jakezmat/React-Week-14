15659287529f4598838aa3ac9b37bd58073e2223
jake matillano <jakematillano.z@gmail.com>
2022-09-10 19:54:20 -0700

    alldone

--- 3day/01-react-hooks-useState/src/components/UseState/index.js
+++ 3day/01-react-hooks-useState/src/components/UseState/index.js
@@ -4,15 +4,17 @@ import "./UseState.css";
 const UseState = () => {
   const [theme,setTheme] = useState("light");
   const [count, setCount] = useState(0)
+
   return (
     <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
       <h2>Current Theme: {theme}</h2>
-      <button onClick={() => setTheme('light')}>Light</button>
-      <button onClick={() => setTheme('dark')}>Dark</button>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount(count+1)}>Increment</button>
-      <button onClick={() => setCount(count-1)}>Downvote</button>
+      <button className={theme==='light'?'dark':'light'} onClick={() => setTheme('light')}>Light</button>
+      <button className={theme==='light'?'dark':'light'}onClick={() => setTheme('dark')}>Dark</button>
+      <button className={theme==='light'?'dark':'light'}onClick={() => theme === 'dark' ? setTheme('light'):setTheme('dark')}>Theme Switcher</button>
+      <h2 >Count: {count}</h2>
+      <button className={theme==='light'?'dark':'light'}onClick={() => setCount((prevCount)=> prevCount+1)}>Inckermont</button>
+      <button className={theme==='light'?'dark':'light'}onClick={() => setCount((prevCount)=> prevCount-1)}>Deckermont</button>
 
     </div>
   );
